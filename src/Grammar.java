8e7902fdfdd56228d2201028df618b4ed586629f
Kenia Rioja <kdrioja@gmail.com>
2018-11-22 13:31:34 -0800

    Added Driver.java

--- src/Grammar.java
+++ src/Grammar.java
@@ -7,45 +7,47 @@ import javafx.util.Pair;
 
 public class Grammar {
     // G = (V, T, S, P)
-    private ArrayList<String> Variables;
-    private ArrayList<String> Terminals;
-    private String S; //Start Symbol
-    private ArrayList<Pair<String, ArrayList<String>>> Productions;
+    private ArrayList<String> variables;
+    private ArrayList<String> terminals;
+    private String start; //Start Symbol
+    private ArrayList<Pair<String, ArrayList<String>>> productions;
 
-    public Grammar() {
-
+    public Grammar(ArrayList<String> v, ArrayList<String> t, String s, ArrayList<Pair<String, ArrayList<String>>> p) {
+        this.variables = v;
+        this.terminals = t;
+        this.start = s;
+        this.productions = p;
     }
 
-
     public ArrayList<String> getVariables() {
-        return Variables;
+        return variables;
     }
 
     public void setVariables(ArrayList<String> variables) {
-        Variables = variables;
+        this.variables = variables;
     }
 
     public ArrayList<String> getTerminals() {
-        return Terminals;
+        return terminals;
     }
 
     public void setTerminals(ArrayList<String> terminals) {
-        Terminals = terminals;
+        this.terminals = terminals;
     }
 
-    public String getS() {
-        return S;
+    public String getStart() {
+        return start;
     }
 
-    public void setS(String s) {
-        S = s;
+    public void setStart(String start) {
+        this.start = start;
     }
 
     public ArrayList<Pair<String, ArrayList<String>>> getProductions() {
-        return Productions;
+        return productions;
     }
 
     public void setProductions(ArrayList<Pair<String, ArrayList<String>>> productions) {
-        Productions = productions;
+        this.productions = productions;
     }
 }
