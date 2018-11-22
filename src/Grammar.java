7bf4a763576291cd8d70349b729f091b619714d3
Kenia Rioja <kdrioja@gmail.com>
2018-11-22 13:11:46 -0800

    Working on Grammar class.

--- src/Grammar.java
+++ src/Grammar.java
@@ -3,8 +3,49 @@
     CSC 471 Project 2
  */
 
+import javafx.util.Pair;
+
 public class Grammar {
     // G = (V, T, S, P)
-    private ArrayList<String> V;
-    
+    private ArrayList<String> Variables;
+    private ArrayList<String> Terminals;
+    private String S; //Start Symbol
+    private ArrayList<Pair<String, ArrayList<String>>> Productions;
+
+    public Grammar() {
+
+    }
+
+
+    public ArrayList<String> getVariables() {
+        return Variables;
+    }
+
+    public void setVariables(ArrayList<String> variables) {
+        Variables = variables;
+    }
+
+    public ArrayList<String> getTerminals() {
+        return Terminals;
+    }
+
+    public void setTerminals(ArrayList<String> terminals) {
+        Terminals = terminals;
+    }
+
+    public String getS() {
+        return S;
+    }
+
+    public void setS(String s) {
+        S = s;
+    }
+
+    public ArrayList<Pair<String, ArrayList<String>>> getProductions() {
+        return Productions;
+    }
+
+    public void setProductions(ArrayList<Pair<String, ArrayList<String>>> productions) {
+        Productions = productions;
+    }
 }
